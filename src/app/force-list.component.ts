900de08a17f3526efb90702bb26ad0c26b8dbbbe
coco83algo <cganzin22@gmail.com>
2020-11-21 11:24:51 +0100

    Création force-list component

---
+++ src/app/force-list.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'cpa-force-list',
+  template: `
+    <p>
+      force-list works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class ForceListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
