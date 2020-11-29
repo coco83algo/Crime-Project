3aa277135767f9eb5a703f9e6b6de867362c0ea0
fili2b <therese.fili@isen.yncrea.fr>
2020-11-29 15:04:00 +0100

    Ajout du html dans force-list-component pour afficher les officiers quand on appuie sur le bouton correspondant

--- src/app/Components/force-details-component/force-details.component.ts
+++ src/app/Components/force-details-component/force-details.component.ts
@@ -1,24 +1,29 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Force } from '../../Interfaces/Force';
+import { ForcesListService } from '../../Services/forces-list-service/forces-list.service';
 
 @Component({
   selector: 'cpa-force-details',
   template: `
-    <p>get details</p>
-    <!-- Pour l'instant je ne l'ai pas mis parce que ça ne fonctionne pas
+    <p>Get details</p>
+    <!--<button (click)="getForceDetails(force)">Details</button>
+    <button (click)="getForceOfficers(force)">Officers</button>
+     Pour l'instant je ne l'ai pas mis parce que ça ne fonctionne pas
     Mais ça sera la forme finale pour les détails d'une force affichée
-    <div>Unique force identifier : {{force.id}} </div>
-    <div>Forn name : {{force.name}} </div>
-    <div>Description : {{force.description}} </div>
-    <div>Force website URL : {{force.url}} </div>
-    <div>Force telephone number : {{force.telephone}} </div> -->
+    <span>Unique force identifier : {{force.id}} </span>
+    <span>Forn name : {{force.name}} </span>
+    <span>Description : {{force.description}} </span>
+    <span>Force website URL : {{force.url}} </span>
+    <span>Force telephone number : {{force.telephone}} </span>-->
   `,
   styles: [],
 })
 
 export class ForceDetailsComponent implements OnInit {
   @Input() force!: Force;
-  constructor() {}
+  @Input() displayDetails!: boolean;
+
+  constructor(private forceslistService: ForcesListService) {}
 
   ngOnInit(): void {}
 }
--- src/app/Components/force-list-component/force-list.component.ts
+++ src/app/Components/force-list-component/force-list.component.ts
@@ -1,5 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Force } from '../../Interfaces/Force';
+import { ForceDetails } from '../../Interfaces/ForceDetails';
+import { ForceOfficer } from '../../Interfaces/ForceOfficer';
 import { ForcesListService } from '../../Services/forces-list-service/forces-list.service';
 import { HttpClient } from '@angular/common/http';
 
@@ -13,39 +15,48 @@ import { HttpClient } from '@angular/common/http';
       </li>
     </ul>-->
     <div>
-    <button (click)="getForces()">Police</button>
-    </div>
+    <button (click)="getForces()">Show all Forces</button>
     <ul>
        <li *ngFor="let currentForce of forceList">
-       {{ currentForce.id }} - {{ currentForce.name | titlecase }}
-       <button (click)="getForceDetails(currentForce)">Details</button>
-       <button (click)="getForceOfficers(currentForce)">Officers</button>
+          <cpa-force [force]="currentForce"></cpa-force>
+          <button (click)="getForceDetails(currentForce)">Details</button>
+          <button (click)="getForceOfficers(currentForce)">Officers</button>
        </li>
     </ul>
-
+    </div>
+        <ul>
+          <li *ngFor="let currentForceOfficer of forceListOfficers">
+              Name: {{ currentForceOfficer.name }}
+              Rank: {{ currentForceOfficer.rank }}
+              Bio: {{ currentForceOfficer.bio }}
+          </li>
+        </ul>
   `,
-  styles: [],
+  styles: []
 })
 
 export class ForceListComponent implements OnInit {
   forceList: Force[] = [];
+  forceListDetails: ForceDetails[] = [];
+  forceListOfficers: ForceOfficer[] = [];
   forces!: Force[];
+  isOfficerDisplay = false;
 
   constructor(private forceslistService: ForcesListService, private httpClient: HttpClient) {}
 
   ngOnInit(): void {
   }
 
-  getForces(){
+  getForces(): void {
     this.forceList = this.forceslistService.getForceFromServer();
   }
 
-  getForceDetails(currentForce: Force){
-    this.forceslistService.getForceDetailsFromServer(currentForce.id);
+  getForceDetails(currentForce: Force): void{
+    this.forceListDetails = this.forceslistService.getForceDetailsFromServer(currentForce.id);
   }
 
-  getForceOfficers(currentForce: Force){
-    this.forceslistService.getForceOfficersFromServer(currentForce.id);
+  getForceOfficers(currentForce: Force): void{
+    this.forceListOfficers = this.forceslistService.getForceOfficersFromServer(currentForce.id);
   }
 
 }
