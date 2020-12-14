7e5aabc2de42dfc39a04a3caf2a193c5e39f2c54
coco83algo <cganzin22@gmail.com>
2020-12-14 18:00:31 +0100

    Map

--- src/app/Components/map-component/map.component.ts
+++ src/app/Components/map-component/map.component.ts
@@ -20,12 +20,17 @@ export class MapComponent implements OnInit {
   options = {
     layers: [
       tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution: '&copy; OpenStreetMap contributors'
+        attribution: '&copy; OpenStreetMap contributors', //
+        maxZoom: 16, // Zoom max que l'utilisateur peut faire
+        minZoom: 6 // Zoom min que l'utilisateur peut faire
       })
     ],
-    zoom: 7,
+    zoom: 10,
     center: latLng([ 51.5084, -0.1255 ])
   };
+  // onMapReady(map) {
+
+  // }
 
   ngOnInit(): void {
   }
