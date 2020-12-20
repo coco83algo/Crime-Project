3d1a5184664e2970cf394a9af2f5312a5070f8db
fili2b <therese.fili@isen.yncrea.fr>
2020-12-20 17:51:43 +0100

    Créations des fichiers html pour organiser les components

--- src/app/Components/force-details-component/force-details.component.ts
+++ src/app/Components/force-details-component/force-details.component.ts
@@ -6,49 +6,7 @@ import {Location} from '@angular/common';
 
 @Component({
   selector: 'cpa-force-details',
-  template: `
-    <div *ngIf="forceDetail">
-      <cpa-navbar></cpa-navbar>
-          <div class="line">
-        <div class="container left">
-          <div class="content">
-            <h2>Unique force identifier</h2>
-            <p>{{forceDetail.id}}</p>
-          </div>
-        </div>
-        <div class="container right">
-          <div class="content">
-            <h2>Forn name</h2>
-            <p>{{forceDetail.name}}</p>
-          </div>
-        </div>
-        <div class="container left">
-          <div class="content">
-            <h2>Description</h2>
-            <div [innerHtml]="forceDetail.description"></div>
-          </div>
-        </div>
-        <div class="container right">
-          <div class="content">
-            <h2>Website and telephone</h2>
-            <p>{{forceDetail.url}}</p>
-            <p>{{forceDetail.telephone}}</p>
-          </div>
-        </div>
-        <div class="container left">
-          <div class="content">
-            <h2>Engagement methods</h2>
-            <div *ngFor="let methods of forceDetail.engagement_methods">
-              <b>{{methods.title}}</b>
-              <p [innerHtml]="forceDetail.description"></p>
-              <p class="urlmethod">{{methods.url}}</p>
-              <p></p>
-          </div>
-        </div>
-      </div>
-    </div>
-    <div class="return"><button class="btn" (click)="backClicked()">Go back to forces</button></div>
-  `,
+  templateUrl: 'forceDetails.html',
   styleUrls: ['forceDetails.css']
 })
 
@@ -66,6 +24,5 @@ export class ForceDetailsComponent implements OnInit {
     const force = params.get('currentForce');
     this.forceslistService.getForceDetailsFromServer(force).subscribe(forceDetail => (this.forceDetail = forceDetail));
     });
-    //console.log(this.forceDetail);
   }
 }
--- src/app/Components/force-list-component/force-list.component.ts
+++ src/app/Components/force-list-component/force-list.component.ts
@@ -7,44 +7,13 @@ import { Observable } from 'rxjs';
 
 @Component({
   selector: 'cpa-force-list',
-  template: `
-  <cpa-navbar></cpa-navbar>
-    <h1 class="text-center">This is the list of forces in this area :</h1>
-    <div class="affiche">
-      <div *ngFor="let currentForce of forceList | async">
-      <div class="column">
-        <div class="flip-card">
-          <div class="flip-card-inner">
-              <div class="flip-card-front">
-                <div><img src="../../../assets/Images/PoliceUK/{{currentForce.id}}.jpg" alt="Insigne {{currentForce.id}}" width="65%"/></div>
-              </div>
-              <div class="flip-card-back">
-                <cpa-force [force]="currentForce"></cpa-force>
-                <div>
-                  <a [routerLink]="['/forces', currentForce.id]">
-                    <button class="btn">Details</button>
-                  </a>
-                  <a [routerLink]="['/forces', currentForce.id, 'officers']">
-                    <button class="btn">Officers</button>
-                  </a>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  `,
-styleUrls: ['forces.css']
+  templateUrl: 'forceList.html',
+  styleUrls: ['forces.css']
 })
 
 export class ForceListComponent implements OnInit {
   forceList!: Observable<Force[]>;
-  forceListDetails!: Observable<ForceDetails>;
-  forceListOfficers!: Observable<ForceOfficer[]>;
   forces!: Force[];
-  selected!: Force;
-  selectedOfficer!: Force;
 
   constructor(private forceslistService: ForcesListService) {}
 
@@ -55,17 +24,4 @@ export class ForceListComponent implements OnInit {
   getForces(): void {
     this.forceList = this.forceslistService.getForceFromServer();
   }
-
-  /*getForceDetails(currentForce: Force): void{
-    this.forceListDetails = this.forceslistService.getForceDetailsFromServer(currentForce.id);
-    this.selected = currentForce;
-    // console.log(this.selected);
-  }
-
-  getForceOfficers(currentForce: Force): void{
-    this.forceListOfficers = this.forceslistService.getForceOfficersFromServer(currentForce.id);
-    this.selectedOfficer = currentForce;
-    // console.log(this.selectedOfficer);
-  }*/
-
 }
