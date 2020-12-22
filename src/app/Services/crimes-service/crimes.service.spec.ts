a87dc975d9e7e3365c72b740f28a02ea03eafa06
fili2b <therese.fili@isen.yncrea.fr>
2020-12-22 10:59:16 +0100

    Début des tests

--- src/app/Services/crimes-service/crimes.service.spec.ts
+++ src/app/Services/crimes-service/crimes.service.spec.ts
@@ -1,11 +1,14 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { TestBed } from '@angular/core/testing';
-
+import { Crime } from '../../Interfaces/Crime';
+import { CrimeStatus } from '../../Interfaces/CrimeStatus';
 import { CrimesService } from './crimes.service';
 
 describe('CrimesService', () => {
   let service: CrimesService;
+  let httpClientSpy: { get: jasmine.Spy };
 
-  beforeEach(() => {
+/*  beforeEach(() => {
     TestBed.configureTestingModule({});
     service = TestBed.inject(CrimesService);
   });
@@ -13,4 +16,42 @@ describe('CrimesService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+});*/
+
+/*beforeEach(() => {
+  // TODO: spy on other methods too
+  httpClientSpy = jasmine.createSpyObj('HttpClient', ['get']);
+  service = new CrimesService(httpClientSpy as any);
 });
+
+it('should return expected crimes (HttpClient called once)', () => {
+  const expectedCrimes: Crime[] =
+    [{ category: 'A',
+      persistent_id: 'A',
+      location_subtype: 'A',
+      id: 'A',
+      location: 'A',
+      context: 'A',
+      month: 08-2020,
+      location_type: 'A',
+      outcome_status: {category: 'A', date: 08-2020},
+    },
+     {  category: 'A',
+      persistent_id: 'A',
+      location_subtype: 'A',
+      id: 'A',
+      location: 'A',
+      context: 'A',
+      month: 08-2020,
+      location_type: 'A',
+      outcome_status: {category: 'A', date: 08-2020},
+     }];
+
+});
+
+it('should return an error when the server returns a 404', () => {
+  const errorResponse = new HttpErrorResponse({
+    error: 'test 404 error',
+    status: 404, statusText: 'Not Found'
+  });
+});})*/
